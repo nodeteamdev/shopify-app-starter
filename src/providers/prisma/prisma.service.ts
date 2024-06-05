27c80b61201df2f3fe0fb7592c8fe24d5ece0584
Mykola Balielov <mykola.balielov@onix-systems.com>
2024-06-05 13:17:58 +0300

    feat(set up docker config): connect prisma and redis

--- src/providers/prisma/prisma.service.ts
+++ src/providers/prisma/prisma.service.ts
@@ -5,7 +5,6 @@ import {
   OnModuleInit,
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-// import { DMMF, DMMFClass } from '@prisma/client/runtime';
 
 
 @Injectable()
@@ -27,8 +26,4 @@ export default class PrismaService extends PrismaClient implements OnModuleInit 
       await app.close();
     });
   }
-
-  // public getModelMap(): { [key: string]: DMMF.Model } {
-  //   return ((this as any)._baseDmmf as DMMFClass).getModelMap();
-  // }
 }
--- src/providers/redis/interfaces/user-redis-data.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IUserSlotsData {
-  readonly sessionId: string;
-  readonly refreshToken: string;
-  readonly accessToken: string;
-  readonly isBlocked: boolean;
-}
