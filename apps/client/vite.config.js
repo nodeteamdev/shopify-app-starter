519feb40d6998872c106a7bd1565e3acc5c475f6
Vladyslav317 <vladyslav3177@gmail.com>
2024-07-26 11:17:19 +0300

    feat(serve-static): remove proxy and add serve-static

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import https from "https";
 import react from "@vitejs/plugin-react";
+
 dotenv.config({ path: ".env" });
 
 if (
@@ -16,33 +16,6 @@ if (
   );
 }
 
-const proxyOptions = {
-  target: `http://127.0.0.1:${process.env.SERVER_PORT}`,
-  changeOrigin: false,
-  secure: true,
-  ws: false,
-};
-
-const host = process.env.SHOPIFY_APP_URL
-  ? process.env.SHOPIFY_APP_URL.replace(/https?:\/\//, "")
-  : "localhost";
-
-let hmrConfig;
-if (host === "localhost") {
-  hmrConfig = {
-    protocol: "ws",
-    host: "localhost",
-    port: 64999,
-    clientPort: 64999,
-  };
-} else {
-  hmrConfig = {
-    protocol: "wss",
-    host: host,
-    port: process.env.CLIENT_PORT,
-    clientPort: 443,
-  };
-}
 export default defineConfig({
   root: dirname(fileURLToPath(import.meta.url)),
   plugins: [react()],
@@ -54,11 +27,6 @@ export default defineConfig({
   },
   server: {
     host: "localhost",
-    port: process.env.CLIENT_PORT,
-    hmr: hmrConfig,
-    proxy: {
-      "^/(\\?.*)?$": proxyOptions,
-      "^/api(/|(\\?.*)?$)": proxyOptions,
-    },
+    hmr: false,
   },
 });
